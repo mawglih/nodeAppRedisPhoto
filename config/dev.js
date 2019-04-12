2fcebc20173615c67d82d8954a707667c5b65795
Oleg Markoff <mawglih@gmail.com>
2019-04-11 22:59:32 -0400

    remobe

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID: '844670040214-73omucb3up2lgnufeges9l0esafi2t63.apps.googleusercontent.com',
-  googleClientSecret: 'uhwsCQxypZVwWbA0e_HuIaYv',
-  mongoURI: 'mongodb://oleg:oleg1967@ds017636.mlab.com:17636/blog-app',
-  cookieKey: '123123123',
-  accessKeyId: 'AKIA3ZJHZEARTUCGHCU4',
-  secretAccessKey: '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'
-};
